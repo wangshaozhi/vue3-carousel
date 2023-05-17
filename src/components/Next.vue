fe40bcfcf29d27e5e4af7ee2f20e35d358baec33
wnagshaozhi <wsz2246500267@163.com>
2023-05-17 23:02:57 +0800

    添加next分支，新增代码

---
+++ src/components/Next.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface IState {
+    msg: string;
+}
+import {reactive} from 'vue';
+const state: IState = reactive({
+    msg: 'Hello Next'
+});
+</script>
+
+<template>
+    <div class="msg">
+        {{ state.msg }}
+    </div>
+</template>
+
+<style scoped>
+.msg {
+    color: red;
+}
+</style>
